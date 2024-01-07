58eb523688c48194c39b51b187f8f78328e787ea
morgan.li <morgan.li@uwaterloo.ca>
2024-01-07 14:04:18 -0500

    types

--- hello-world/helloworld.go
+++ hello-world/helloworld.go
@@ -28,8 +28,72 @@ func printEmoji() {
 	fmt.Printf(emoji)
 }
 
+func basicType() {
+	var a int = 10
+	// define type when declear variable
+	// a cannot be string or other type
+	fmt.Println(a)
+	var b = 12
+	// b can not be any type either
+	// init as string so it must be string
+	fmt.Println(b)
+	var s = "hello world"
+	fmt.Println(s)
+	var c = 'a'
+	fmt.Println(c)
+	var bo = true
+	fmt.Println(bo)
+	var f = 1.2
+	fmt.Println(f)
+	// fixed size array
+	var arr = [3]int{1, 2, 3}
+	fmt.Println(arr)
+	// slice, dynamic size array
+	var arr2 = [...]int{1, 2, 3}
+	fmt.Println(arr2)
+	var myMap = map[string]int{"a": 1, "b": 2}
+	fmt.Println(myMap)
+	var map2 = make(map[string]int)
+	fmt.Println(map2)
+	var slice = []int{1, 2, 3}
+	fmt.Println(slice)
+	var slice2 = make([]int, 3)
+	fmt.Println(slice2)
+	// init with 3 element ane have capacity of 5
+	var slice3 = make([]int, 3, 5)
+	fmt.Println(slice3)
+	var slice4 = arr[0:2]
+	fmt.Println(slice4)
+	var slice5 = arr[1:]
+	fmt.Println(slice5)
+	var slice6 = arr[:2]
+	fmt.Println(slice6)
+	var slice7 = append(slice, 4)
+	fmt.Println(slice7)
+	var slice8 = append(slice, slice2...)
+	fmt.Println(slice8)
+
+	// name is a type and it is same as string
+	type name string
+
+	// convert int to float64
+	var i int = 42
+	var f2 float64 = float64(i)
+	fmt.Println(f2)
+
+	// interface{} is any type
+	var ix = interface{}(42)
+	fmt.Println(ix)
+
+	//get type by interface{}
+	var is interface{} = "hello"
+	si := is.(string)
+	fmt.Println(si)
+}
+
 func main() {
 	helloworld()
 	printf()
 	printEmoji()
+	basicType()
 }
