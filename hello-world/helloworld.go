245f4f5dddc06a29364a1c4fcdaeafb40df395db
morgan.li <morgan.li@uwaterloo.ca>
2024-01-07 11:06:50 -0500

    printf

--- hello-world/helloworld.go
+++ hello-world/helloworld.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
+func helloworld() {
 	fmt.Println("Hello, World !")
+}
+
+func printf() {
+	const name, age = "Kim", 22
+	fmt.Printf("%s is %d years old.\n", name, age)
+}
+
+func main() {
+	helloworld()
+	printf()
 }
